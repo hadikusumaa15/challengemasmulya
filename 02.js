0726b09e85eede16e9164dc4c5021ba755a3720d
Taufan Arif <hadikusumaa15@gmail.com>
2018-03-01 21:23:58 +0700

    occa

---
+++ 02.js
@@ -0,0 +1,5 @@
+const first = ['Behind', 'every', 'great', 'man']
+const second = ['is', 'a', 'woman']
+const third = ['rolling', 'her', 'eyes']
+
+console.log (first[0]+' '+first[1]+' '+first[2]+' '+first[3]+' '+second[0]+' '+second[1]+' '+second[2]+' '+third[0]+' '+third[1]+' '+third[2]);
